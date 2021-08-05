f3629d707f0310b18b25f7026cdaa02b2f19de3e
Marc Torné <marc.torne@oracle.com>
2021-08-05 18:10:05 +0200

    configured commit

--- compute.tf
+++ compute.tf
@@ -1,10 +1,10 @@
 resource "oci_core_instance" "ubuntu_instance" {
     # Required
     availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
-    compartment_id = "ocid1.compartment.oc1..aaaaaaaagz2culbclkwhavebagh2q2cb2enom6ka7unly34kpaoswcpxv6hq"
+    compartment_id = "${var.compartment_ocid}"
     shape = "VM.Standard.E2.1"
     source_details {
-        source_id = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaasv3tl5ukgospupqo3hjvdqwcsysz7czullovbnk2fy6f73humu5a"
+        source_id = "${var.InstanceImageOCID[var.region]}"
         source_type = "image"
     }
 
--- provider.tf
+++ provider.tf
@@ -1,7 +1,3 @@
 provider "oci" {
-  tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaaibrpsc2fhjxvmbla6gu327mkldqwaabj37gzhgorkfw4q46tznva"
-  user_ocid = "ocid1.user.oc1..aaaaaaaaarewudaxcrpeexcth7lsnna3hx6xqj6fcnyrhlsigwnh2g3lotla" 
-  private_key_path = "/home/opc/.oci/oracleidentitycloudservice_marc.torne-08-03-16-14.pem"
-  fingerprint = "92:e9:fd:e1:9a:ad:79:1e:9c:fe:80:02:14:4e:e1:fc"
-  region = "eu-frankfurt-1"
+  region = "${var.region}"
 }
